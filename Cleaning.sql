471f4f2353af5fb3216dc01c4c24e59edb41b1e5
M Abdul Fazal <mmohd.abdul.fazal@gmail.com>
2024-08-06 19:54:52 +0530

    Dublicating the Data from Raw Data File

--- Cleaning.sql
+++ Cleaning.sql
@@ -1,1 +1,19 @@
-select * from layoffs;
+-- Data Cleaning
+
+Select * From layoffs;
+
+-- 1. Remove Dublicates
+-- 2. Standardize the Data
+-- 3. Null Values or Blank values
+-- 4. Remove Any Columns 'Unnessory'
+
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+
+Select * From layoffs_staging;
+
+
+INSERT layoffs_staging
+SELECT *
+FROM layoffs; 
